d1f2b3feb762c8716ce1d277c2636a11fa92029c
Gonzalo Martinez <gonzalom5@users.noreply.github.com>
2026-09-25 04:48:43 -0300

    feat: add routed static website

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
+// Imports
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+/**
+ * Declare Important Variables
+ */
+
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Setup Express Server
+ */
 
 const app = express();
 
-const name = process.env.NAME;
+/**
+ * Configure Express Middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Declare Routes
+ */
 
 app.get("/", (req, res) => {
-  res.send(`Welcome, ${name}!`);
+  res.sendFile(path.join(__dirname, "src/views/home.html"));
 });
 
-const PORT = 3000;
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/about.html"));
+});
 
+app.get("/products", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/views/products.html"));
+});
+
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
